3237acc744852d3feeddd9f56316f866c01d814b
Hagar Elbakry <hagarelbakry12@gmail.com>
2026-03-03 11:38:38 +0200

    add button to display current time

---
+++ main.js
@@ -0,0 +1,21 @@
+let btn = document.getElementById('btn');
+
+btn.onclick = function() {
+    let currentTime = new Date();
+    let hours = currentTime.getHours();
+    let minutes = currentTime.getMinutes();
+    let seconds = currentTime.getSeconds();
+
+    alert(`Current Time: ${hours}:${minutes}:${seconds}`);
+};
+
+//                      OR
+
+// btn.addEventListener('click', function() {
+// let currentTime = new Date();
+//     let hours = currentTime.getHours();
+//     let minutes = currentTime.getMinutes();
+//     let seconds = currentTime.getSeconds();
+
+//     alert(`Current Time: ${hours}:${minutes}:${seconds}`);
+// });
